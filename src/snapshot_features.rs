34b239b7caef4f9ff2db98118daeb657898fb6f1
rdsqC <pdiamondq49@gmail.com>
2025-12-12 20:34:41 +0900

    特徴抽出のイテレーター作成のトレイト実装完了 #5

--- src/snapshot_features.rs
+++ src/snapshot_features.rs
@@ -1,5 +1,5 @@
-use crate::bitboard::{BITBOD_WIDTH, FIELD_BOD, FIELD_BOD_WIDTH, MoveBit};
-use crate::snapshot::{self, BoardSnapshot};
+use crate::bitboard::{BITBOD_WIDTH, FIELD_BOD, FIELD_BOD_WIDTH};
+use crate::snapshot::BoardSnapshot;
 use std::arch::x86_64::_pext_u64;
 
 macro_rules! build_features {
@@ -66,6 +66,9 @@ const ZERO_CONFIG: LineConfig = LineConfig {
     offset: 0,
 };
 
+const NUM_LINE3: usize = 4;
+const NUM_LINE4: usize = 4;
+const NUM_LINE5: usize = 20;
 pub const FEATURE_LINES: [LineConfig; 20] = {
     let mut masks = [0u64; 20];
 
@@ -135,6 +138,7 @@ pub const FEATURE_LINES: [LineConfig; 20] = {
         sq_idx += 1;
     }
 
+    //sort ascending order
     let mut i = 0;
     while i < 20 {
         let mut j = 0;
@@ -175,6 +179,148 @@ struct LineConfig {
     mask: u64,
     length: usize,
     offset: usize,
+}
+
+const MAX_LINE_LEN: usize = 5;
+const TABLE_SIZE: usize = 1 << (MAX_LINE_LEN * 2);
+
+static TERNARY_LUT: [u16; TABLE_SIZE] = generate_ternary_table();
+
+const fn generate_ternary_table() -> [u16; TABLE_SIZE] {
+    let mut table = [0; TABLE_SIZE];
+    let mut i = 0;
+
+    while i < TABLE_SIZE {
+        let a = (i as u64) & ((1 << MAX_LINE_LEN) - 1);
+        let b = (i as u64) >> MAX_LINE_LEN;
+
+        if (a & b) != 0 {
+            table[i] = 0;
+        } else {
+            let mut val = 0;
+            let mut weight = 1;
+            let mut bit = 0;
+
+            while bit < MAX_LINE_LEN {
+                let bit_a = ((a >> bit) & 1) as u16;
+                let bit_b = ((b >> bit) & 1) as u16;
+
+                val += (bit_a * bit_b * 2) * weight;
+                weight *= 3;
+                bit += 1;
+            }
+            table[i] = val;
+        }
+        i += 1;
+    }
+    table
+}
+
+#[inline(always)]
+fn encode_ternary_lut(a: u64, b: u64) -> usize {
+    debug_assert!((a & b) == 0, "a and b must not overlap");
+
+    let index = (a | (b << MAX_LINE_LEN)) as usize;
+
+    TERNARY_LUT[index] as usize
+}
+
+//2駒関係。ただし同じ駒同士の関係も含む
+struct PPFeatures;
+
+impl FeatureGroup for PPFeatures {
+    const LEN: usize = 325;
+    fn get_iter(snapshot: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        let p1_packed = unsafe { _pext_u64(snapshot.p1, FIELD_BOD) };
+        let p2_packed = unsafe { _pext_u64(snapshot.p2, FIELD_BOD) };
+
+        let p1_iter = BitIter(p1_packed);
+        let p2_iter = BitIter(p2_packed).map(|idx| idx + NUM_VALID_SQUARES);
+        let p1p2_iter = p1_iter.chain(p2_iter);
+
+        let pp_iter = p1p2_iter.clone().flat_map(move |sq1| {
+            p1p2_iter
+                .clone()
+                .filter(move |&sq2| sq1 <= sq2)
+                .map(move |sq2| offset_set + (sq1 * NUM_VALID_SQUARES + sq2))
+        });
+
+        pp_iter
+    }
+}
+
+//LINE
+struct LineFeatures;
+
+impl FeatureGroup for LineFeatures {
+    const LEN: usize = 3348;
+    fn get_iter(snapshot: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        FEATURE_LINES.iter().map(move |line_config| {
+            let white_line: u64 = unsafe { _pext_u64(snapshot.p1, line_config.mask) };
+            let black_line: u64 = unsafe { _pext_u64(snapshot.p2, line_config.mask) };
+            offset_set + line_config.offset + encode_ternary_lut(white_line, black_line)
+        })
+    }
+}
+
+//Bias
+struct BiasFeatures;
+
+impl FeatureGroup for BiasFeatures {
+    const LEN: usize = 1;
+    fn get_iter(_: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        Some(offset_set).into_iter()
+    }
+}
+
+//Turn
+struct TurnFeatures;
+
+impl FeatureGroup for TurnFeatures {
+    const LEN: usize = 1;
+    fn get_iter(snapshot: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        if snapshot.turn == 1 {
+            Some(offset_set).into_iter()
+        } else {
+            None.into_iter()
+        }
+    }
+}
+
+//HandPiece
+struct HandPieceFeatures;
+
+impl FeatureGroup for HandPieceFeatures {
+    const LEN: usize = 12;
+    fn get_iter(snapshot: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        [
+            offset_set + snapshot.p1_hand_piece as usize,
+            offset_set + snapshot.p2_hand_piece as usize,
+        ]
+        .into_iter()
+    }
+}
+
+//PrevMove
+struct PrevMoveFeatures;
+
+impl FeatureGroup for PrevMoveFeatures {
+    const LEN: usize = 225;
+    fn get_iter(snapshot: &BoardSnapshot, offset_set: usize) -> impl Iterator<Item = usize> {
+        snapshot
+            .prev_move
+            .map(|mv| {
+                if mv.angle_idx < 8 {
+                    offset_set
+                        + NUM_VALID_SQUARES
+                        + mv.field_idx() as usize
+                        + NUM_VALID_SQUARES * mv.angle_idx as usize
+                } else {
+                    offset_set + mv.field_idx() as usize
+                }
+            })
+            .into_iter()
+    }
 }
 
 pub const NUM_FEATURES: usize = 289;
